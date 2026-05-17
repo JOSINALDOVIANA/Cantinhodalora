bdc3663100113f94e32fa33cad80a7cf15b148d7
josinaldoviana <josinaldo60@gmail.com>
2026-05-17 02:34:53 -0300

    Atualiza backend

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const url = "http://108.175.7.54:3001/";
+export const url = "https://www.cantinhodalora.info";
 export const api = axios.create({
     baseURL: url,
     // http://109.123.243.212:3001/
